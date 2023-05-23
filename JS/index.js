3467cc9a9c6d86832eca8adaa783aa42ca8768ea
Lina Zerkly <zerkly.lina@gmail.com>
2023-05-23 12:41:28 +0200

    obstacles smoother

--- JS/index.js
+++ JS/index.js
@@ -3,8 +3,8 @@ class Game {
   constructor() {
     this.rock;
     this.obstaclesArr = [];
-    this.bottomObstArr = []
-    this.addEventListener()
+    this.bottomObstArr = [];
+    this.addEventListener();
   }
 
   start() {
@@ -13,36 +13,32 @@ class Game {
 
     setInterval(() => {
       const newObstacle = new TopObstacle();
-      this.obstaclesArr.push(newObstacle)
+      this.obstaclesArr.push(newObstacle);
     }, 3000);
-  
 
     setInterval(() => {
-        const newObstacle = new BottomObstacle();
-        this.bottomObstArr.push(newObstacle)
-      }, 3000);
-    
+      const newObstacle = new BottomObstacle();
+      this.bottomObstArr.push(newObstacle);
+    }, 3000);
 
     setInterval(() => {
-    this.obstaclesArr.forEach((obstacleInstance) => {
+      this.obstaclesArr.forEach((obstacleInstance) => {
         obstacleInstance.moveLeft();
-    
-    });
-}, 60);
+      });
+    }, 60);
 
-setInterval(() => {
-    this.bottomObstArr.forEach((obstacleInstance) => {
+    setInterval(() => {
+      this.bottomObstArr.forEach((obstacleInstance) => {
         obstacleInstance.moveLeft();
-    
-    });
-}, 60);
-}
+      });
+    }, 60);
+  }
 
   addEventListener() {
     console.log("listeners");
     document.addEventListener("keydown", (event) => {
-    console.log("event", event)
-    if (event.code === "ArrowUp") {
+      console.log("event", event);
+      if (event.code === "ArrowUp") {
         this.rock.moveUp();
       } else if (event.code === "ArrowDown") {
         this.rock.moveDown();
@@ -50,7 +46,6 @@ setInterval(() => {
     });
   }
 }
-
 
 // create a player class with positioning height width ...
 
@@ -60,14 +55,17 @@ class Rock {
     this.height = 20;
     this.positionX = 0;
     this.positionY = 50;
+    
+
+
   }
 
   // create a dom element
   rockPlayer() {
     this.rock = document.createElement("div");
     this.rock.id = "rock";
-    this.rock.style.width = 40 + "px";
-    this.rock.style.height = 40 + "px" ;
+    this.rock.style.width = 60 + "px";
+    this.rock.style.height = 40 + "px";
     this.rock.style.left = this.positionX + "vw";
     this.rock.style.top = this.positionY + "vh";
 
@@ -77,16 +75,15 @@ class Rock {
 
   // implementing up/down movement
   moveUp() {
-
     this.positionY--;
     this.rock.style.top = this.positionY + "vh";
-    console.log("moveup", this.positionY)
+    console.log("moveup", this.positionY);
   }
 
   moveDown() {
     this.positionY++;
     this.rock.style.top = this.positionY + "vh";
-    console.log("moveD", this.positionY)
+    console.log("moveD", this.positionY);
   }
 }
 
@@ -96,10 +93,8 @@ class TopObstacle {
   constructor() {
     this.width = 1;
     this.height = Math.floor(Math.random() * 100) - 20;
-    this.positionX = 50 ;
-    this.positionY = 0 ;
-    
-
+    this.positionX = 50;
+    this.positionY = 0;
 
     this.createObstacle();
   }
@@ -111,7 +106,6 @@ class TopObstacle {
     this.obstacle.style.height = this.height + "vh";
     this.obstacle.style.right = this.positionX + "vw";
     this.obstacle.style.top = this.positionY + "vh";
-    
 
     const parentElement = document.getElementById("board");
     parentElement.appendChild(this.obstacle);
@@ -124,34 +118,32 @@ class TopObstacle {
 }
 
 class BottomObstacle {
-    constructor() {
-      this.width = 1;
-      this.height = Math.floor(Math.random() * 100) - 20;
-      this.positionX = 100 ;
-      this.positionY = 0 ;
-      
-  
-      this.createObstacle();
-    }
-  
-    createObstacle() {
-      this.obstacle = document.createElement("div");
-      this.obstacle.className = "obstacle";
-      this.obstacle.style.width = this.width + "vw";
-      this.obstacle.style.height = this.height + "vh";
-      this.obstacle.style.right = this.positionX + "vw";
-      this.obstacle.style.top = this.positionY + "vh";
-      
-  
-      const parentElement = document.getElementById("board");
-      parentElement.appendChild(this.obstacle);
-    }
-  
-    moveLeft() {
-      this.positionX--;
-      this.obstacle.style.left = this.positionX + "vw";
-    }
+  constructor() {
+    this.width = 1;
+    this.height = Math.floor(Math.random() * 100) - 20;
+    this.positionX = 100;
+    this.positionY = 0;
+
+    this.createObstacle();
   }
-  
+
+  createObstacle() {
+    this.obstacle = document.createElement("div");
+    this.obstacle.className = "obstacle";
+    this.obstacle.style.width = this.width + "vw";
+    this.obstacle.style.height = this.height + "vh";
+    this.obstacle.style.right = this.positionX + "vw";
+    this.obstacle.style.top = this.positionY + "vh";
+
+    const parentElement = document.getElementById("board");
+    parentElement.appendChild(this.obstacle);
+  }
+
+  moveLeft() {
+    this.positionX--;
+    this.obstacle.style.left = this.positionX + "vw";
+  }
+}
+
 const game = new Game();
-game.start()
+game.start();
